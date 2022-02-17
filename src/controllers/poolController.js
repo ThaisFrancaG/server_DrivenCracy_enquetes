a6a471745cbeacf1c7b67a1250a80b4c2d4cda0e
Thais <tfrancag@gmail.com>
2022-02-17 16:40:20 -0300

    test. Fixed validations on pool post route to prevent criation of duplicates

--- src/controllers/poolController.js
+++ src/controllers/poolController.js
@@ -2,19 +2,27 @@ import db from "../db.js";
 import dayjs from "dayjs";
 
 export async function sendPool(req, res) {
-  const pool = req.body;
+  const { title, expireAt } = req.body;
+  console.log(title);
+  console.log(expireAt);
 
-  console.log(pool);
-  console.log(pool.expireAt);
+  //primeiro, conferir se já tem outra pool com esse mesmo título
+  const checkPool = await db.collection("pools").findOne({ title });
 
-  if (!pool.expireAt) {
+  if (!!checkPool) {
+    return res
+      .status(409)
+      .send("Já tem uma enquete com esse nome. Tente criar outra");
+  }
+
+  if (!expireAt) {
     let currentTime = dayjs().add(30, "day").format("YYYY-MM-D hh:mm");
 
-    const finalPool = { ...pool, expireAt: currentTime };
-
-    console.log(finalPool);
+    const completedPool = { title, expireAt: currentTime };
+    await db.collection("pools").insertOne(completedPool);
+    return res.status(201).send(`Enquete ${title} foi criada!`);
   }
 
   await db.collection("pools").insertOne(pool);
-  return res.status(201).send("ok!");
+  return res.status(201).send(`Enquete ${title} foi criada!`);
 }
