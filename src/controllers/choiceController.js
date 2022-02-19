6d223147e6807cb26c0782ecef455016e7b0c6d9
Thais <tfrancag@gmail.com>
2022-02-19 16:20:03 -0300

    feat. Added path and function for voting in a choice thru the choice's id. Votes are added to chocie object using mongo's

--- src/controllers/choiceController.js
+++ src/controllers/choiceController.js
@@ -6,8 +6,6 @@ export async function sendChoice(req, res) {
   const { title, poolId } = req.body;
   const choice = req.body;
   try {
-    //o formato do ID tem que estar correto. Se não estiver, não funciona dentro do ObjectId
-
     const searchId = ObjectId(poolId);
     //essa parte está certa?
     const currentPool = await db.collection("pools").findOne({ _id: searchId });
@@ -19,7 +17,7 @@ export async function sendChoice(req, res) {
           "Enquete não encontrada. Confira o id informado e o formato do mesmo"
         );
     }
-    //conferir se a enquete em questào já expirou
+
     const poolExpiration = currentPool.expireAt;
     const dateOfChoice = dayjs().format("YYYY-MM-D hh:mm");
     if (dateOfChoice > poolExpiration) {
@@ -27,23 +25,66 @@ export async function sendChoice(req, res) {
         .status(403)
         .send("O prazo para interação nessa enquete já acabou");
     }
-    //conferir se essa opção já foi inclusa
+
     const poolChoices = await db
       .collection("choices")
       .findOne({ title: title });
-    console.log(poolChoices);
 
     if (poolChoices) {
       return res.status(409).send("Título inválido!");
     }
 
-    await db.collection("choices").insertOne(choice);
+    await db.collection("choices").insertOne({ ...choice, votes: 0 });
 
     return res
       .status(201)
       .send(
         `Opção "${title}" adicionada à enquete "${currentPool.title}" com sucesso!`
       );
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+}
+
+export async function addVote(req, res) {
+  const choiceId = req.params.id;
+
+  try {
+    const choice = await db
+      .collection("choices")
+      .findOne({ _id: ObjectId(choiceId) });
+    if (!choice) {
+      return res.status(404).send("Essa não é uma opção válida");
+    }
+
+    const poolId = choice.poolId;
+
+    const choicePool = await db
+      .collection("pools")
+      .findOne({ _id: ObjectId(poolId) });
+
+    if (!choicePool) {
+      return res
+        .status(404)
+        .send("A enquete não foi encontrada. Tente novamente mais tarde");
+    }
+    const poolExpiration = choicePool.expireAt;
+    const dateOfChoice = dayjs().format("YYYY-MM-D hh:mm");
+
+    if (dateOfChoice > poolExpiration) {
+      return res
+        .status(403)
+        .send("O prazo para votação nessa enquete já acabou");
+    }
+
+    await db
+      .collection("choices")
+      .findOneAndUpdate({ _id: ObjectId(choiceId) }, { $inc: { votes: 1 } });
+
+    return res
+      .status(201)
+      .send(`Voto para "${choice.title}" enviado com sucesso`);
   } catch (error) {
     console.log(error);
     return res.sendStatus(500);
--- src/routes/choiceRouter.js
+++ src/routes/choiceRouter.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { sendChoice } from "../controllers/choiceController.js";
+import { sendChoice, addVote } from "../controllers/choiceController.js";
 import { choiceValidate } from "../middleware/choiceValidate.js";
 
 const choiceRouter = Router();
 
 choiceRouter.post("/choice", choiceValidate, sendChoice);
-
+choiceRouter.post("/choice/:id/vote", addVote);
 export default choiceRouter;
